cb865691b16bee8f8b712475714d29cdc119e484
Fellipe <fellipe_sdp@hotmail.com>
2023-04-25 19:57:55 -0300

    final front

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,50 @@
+import { useState } from "react"
+
 export const LoginForm = () => {
+    const [email, SetEmail] = useState(null)
+    const [pass, SetPass] = useState(null)
+
+    const handleEmail = (e) => {
+        SetEmail(e.target.value)
+    }
+
+    const handlePass = (e) => {
+        SetPass(e.target.value)
+    }
+
+    const handleForm = (e) => {
+        e.preventDefault()
+        console.log(email)
+        console.log(pass)
+    }
+
     return (
-        <form className="login-form">
+        <form className="login-form" onSubmit={handleForm}>
             <h3>Login</h3>
             <p>Entre na sua conta abaixo</p>
-            <label className="login-label">Email</label>
-            <input type="text" className="login-input"></input>
-            <label className="login-label">Senha</label>
-            <input type="text" className="login-input"></input>
-            <button className="btn-login">Entrar</button>
+            <label className="login-label" htmlFor="email">
+                Email
+            </label>
+            <input
+                type="text"
+                className="login-input"
+                id="email"
+                onChange={handleEmail}
+                placeholder="Digite seu email"
+            ></input>
+            <label className="login-label" htmlFor="password">
+                Senha
+            </label>
+            <input
+                type="password"
+                className="login-input"
+                id="password"
+                onChange={handlePass}
+                placeholder="Digite sua senha"
+            ></input>
+            <button className="btn-login" type="submit">
+                Entrar
+            </button>
         </form>
     )
 }
